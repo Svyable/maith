ed6b577c42ef0027d360c8ab63eae7d6ed417dbc
Sven Benson <129682319+Svyable@users.noreply.github.com>
2026-09-21 08:03:44 -0500

    Content: register cybersecurity expansion

--- src/content/cybersecurity/index.ts
+++ src/content/cybersecurity/index.ts
@@ -1,2 +1,5 @@
-import { cybersecurityQuestions } from './questions';
-export { cybersecurityQuestions };
+import { cybersecurityQuestions as foundationQuestions } from './questions';
+import { cybersecurityExpansionQuestions } from './expansion';
+
+export const cybersecurityQuestions = [...foundationQuestions, ...cybersecurityExpansionQuestions];
+export { cybersecurityExpansionQuestions };
